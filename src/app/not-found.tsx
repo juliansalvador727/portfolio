903cab88ef3177344d70bc17b74d8cb37a427dcf
Julian Salvador <juliansalvador727@gmail.com>
2025-09-27 00:05:24 -0400

    fixed link issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="mx-auto max-w-xl">
       <h1>404 - page not found</h1>
       <p>
-        <a href="/" className="border-b-2">
+        <Link href="/" className="border-b-2">
           homepage
-        </a>
+        </Link>
       </p>
     </div>
   );
