b2911bd3450b94bc76ee777983c71b0923e32f81
Julian Salvador <juliansalvador727@gmail.com>
2025-09-27 16:23:58 -0400

    logos and fluff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,109 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import UWLogo from "@/components/icons/UWLogo.png";
+import OrbitalLogo from "@/components/icons/Orbital.png";
+import VayaLogo from "@/components/icons/VayaLogo.png";
+import SFULogo from "@/components/icons/SFULogo.png";
+import GojoLogo from "@/components/icons/GojoLogo.png";
 
 export default function Home() {
   return (
     <>
       <main>
         <div className="space-y-1">
-          <p>hi, my name is julian. </p>
+          <p>hi, my name is julian.</p>
           <p>currently, i&apos;m... </p>
-          <ul className="list-disc list-inside">
-            <li>a computer engineering student @ uwaterloo</li>
-            <li>building fullstack for satellites @ uw orbital</li>
+          <ul className="list-none">
+            <li className="flex items-center space-x-1">
+              <span>‣</span>
+              <span>a computer engineering student @</span>
+              <Image src={UWLogo} alt="UW Logo" width={15} height={15} />
+              <span>
+                <Link
+                  className="underline"
+                  href="https://uwaterloo.ca/electrical-computer-engineering/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  uwaterloo
+                </Link>
+              </span>
+            </li>
+            <li className="flex items-center space-x-1">
+              <span>‣</span>
+              <span>doing fullstack & sending a satellite to space @</span>
+              <Image
+                src={OrbitalLogo}
+                alt="Orbital Logo"
+                width={15}
+                height={15}
+              />
+              <span>
+                <Link
+                  className="underline"
+                  href="https://www.uworbital.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  orbital
+                </Link>
+              </span>
+            </li>
           </ul>
+
           <p>in the past i...</p>
-          <ul className="list-disc list-inside">
-            <li>taught stem @ sfu applied sciences</li>
-            <li>won 2 piano concerto competitions @ vayalive</li>
-            <li>colored satoru gojo with math in desmos</li>
+          <ul className="list-none">
+            <li className="flex items-center space-x-1">
+              <span>‣</span>
+              <span>did engineering outreach to 250+ students @</span>
+              <Image src={SFULogo} alt="SFU Logo" width={25} height={20} />
+              <span>
+                <Link
+                  className="underline"
+                  href="https://www.sfu.ca/fas/about/community-engagement-outreach/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Simon Fraser University
+                </Link>
+              </span>
+            </li>
+            <li className="flex items-center space-x-1">
+              <span>‣</span>
+              <span>won 2 piano concerto competitions @</span>
+              <Image src={VayaLogo} alt="Vaya Logo" width={15} height={15} />
+              <span>
+                <Link
+                  className="underline"
+                  href="https://vayafestivals.ca/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  vayalive
+                </Link>
+              </span>
+            </li>
+            <li className="flex items-center space-x-1">
+              <span>‣</span>
+              <span>coloured the greatest jujutsu sorcerer</span>
+              <Image
+                src={GojoLogo}
+                alt="Vaya Logo"
+                width={20}
+                height={20}
+              ></Image>
+              <span>
+                <Link
+                  className="underline"
+                  href="https://www.desmos.com/calculator/9gcwysjqal"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  with math
+                </Link>
+              </span>
+            </li>
           </ul>
         </div>
 
