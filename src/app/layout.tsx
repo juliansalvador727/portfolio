04fbc14f4f9c6258c9de30b795fa3a0140415353
Julian Salvador <juliansalvador727@gmail.com>
2025-09-26 23:19:52 -0400

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/jsicon.svg"></link>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
