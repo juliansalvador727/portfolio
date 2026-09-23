486e46d8d90a45d52d3fdfa3e61eb5d033b538b6
juliansalvador727 <juliansalvador727@gmail.com>
2026-09-22 23:48:53 -0400

    rough draft

---
+++ src/app/writing/coop-one-living/page.tsx
@@ -0,0 +1,19 @@
+/*
+This is the second part of a two part series on my first co-op. This part is about living in and travelling around Europe.
+
+Stuttgart, Deutschland was a great place to live in for four months.
+I flew on May 7, feeling nervous and anxious about being in a completely new environment.
+
+There is a large ruleset in Germany when it came to living. The people there decided a hundred years ago what was socially acceptable and unacceptable
+
+General Culture Shocks / Quirks: (Include the german term when i remember)
+- windows are 80-90% standardized. they have three modes: open fully, open the top part, or lock/closed. it makes windows easy to use everywhere
+- all outside doors on buildings open towards the outside. if you are entering a building/mall, you pull. if you are exiting you push. this is for fire safety
+- no loud noise from 10pm - 6am everyday. no loud noise all day on sunday. this includes partying, repairs, and construction. i had the best sleep of my life in Germany
+- high trust society: no train ticket check to initally get on a train, bus, etc... people leaving their stuff around at beaches, lakes. trust in the fact you follow the contracts you sign, to take days off at work
+- the german stare: the first time i was at a grocery store self checkout, i forgot to take the receipt and scan the barcode to exit, which caused the alarm to go off. following this, EVERYONE in the self checkout stopped what they were doing and lookd at me.
+- "my english isnt that good" proceeds to speak better english than me
+- the directness. no fluff when talking about work or anything just straight to the point.
+
+Some inspiration was taken from https://mertbulan.com/more-german-than-many-germans/
+*/
